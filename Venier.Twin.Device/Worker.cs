fc2d7be22863e8989b961faad749865dc33dfbd2
francescavenier <francescavenier10@gmail.com>
2020-01-17 15:17:49 +0100

    Init

--- Venier.Twin.Device/Worker.cs
+++ Venier.Twin.Device/Worker.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,18 +13,44 @@ namespace Venier.Twin.Device
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _logger.LogInformation("Start device: {time}", DateTimeOffset.Now);
+
+            var deviceClient =
+                    DeviceClient.CreateFromConnectionString(
+                        _configuration.GetConnectionString("iothub"),
+                        TransportType.Amqp);
+
+            //var twin = await deviceClient.GetTwinAsync();
+            //var color = twin.Properties.Desired["color"];
+            //var x = (int)ConsoleColor.Yellow;
+            //Console.BackgroundColor = (ConsoleColor)14;
+            //twin.Properties.Reported["status"] = "ok";
+            //twin.Properties.Reported["dateChancheStatus"] = DateTime.Now.ToString();
+            //await deviceClient.UpdateReportedPropertiesAsync(twin.Properties.Reported);
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                var message = await deviceClient.ReceiveAsync(TimeSpan.FromSeconds(30));
+                if (message != null)
+                {
+                    var text = System.Text.UTF8Encoding.UTF8.GetString(message.GetBytes());
+                    Console.WriteLine("Messaggio: " + text);
+                }
+                else
+                {
+                    await Task.Delay(2000, stoppingToken);
+                }
             }
         }
     }
--- Venier.Twin.WebApp/Pages/Index.cshtml.cs
+++ Venier.Twin.WebApp/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Venier.Twin.WebApp.Pages
@@ -11,15 +14,64 @@ namespace Venier.Twin.WebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public string[] Devices = new string[] { "test1", "test2" };
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string DeviceId { get; set; }
+
+            [Required]
+            public string Text { get; set; }
+        }
+
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
 
+
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var serviceClient = ServiceClient.CreateFromConnectionString(
+                                        _configuration.GetConnectionString("iothub"),
+                                        TransportType.Amqp);
+                var text = System.Text.UTF8Encoding.UTF8.GetBytes(Input.Text);
+                var message = new Message(text);
+
+                await serviceClient.SendAsync(Input.DeviceId, message);
+
+                Message = $"Messaggio inviato al dispositivo {Input.DeviceId} con successo";
+
+                return RedirectToPage();
+            }
+
+
+            //var registry = RegistryManager.CreateFromConnectionString(_configuration.GetConnectionString("iothub"));
+            //var twin = await registry.GetTwinAsync("test1");
+            //twin.Properties.Desired["temperatura"] = 50;
+            //var status = twin.Properties.Reported["status"] as string;
+            //await registry.UpdateTwinAsync("test1", twin, twin.ETag);
+
+            return Page();
         }
     }
 }
